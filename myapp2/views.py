4a771331cf13bcaced9250c727a63eab1214dd04
alfred <lengealfred@gmail.com>
2024-02-05 02:55:35 +0000

    first commit

---
+++ myapp2/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+def wap(request):
+    return render(request,'index.html1')
+
+def wap1(request):
+    return render (request,'index.html2')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
